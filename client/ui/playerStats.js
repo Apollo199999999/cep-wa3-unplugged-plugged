c12858e42527661bbbe8b810093e5465703ad8de
apxy36 <107977710+apxy36@users.noreply.github.com>
2024-05-01 15:08:30 +0800

    lobbies implemented

--- client/ui/playerStats.js
+++ client/ui/playerStats.js
@@ -62,6 +62,41 @@ function updateMuteCondition(frameDocument, muteCondition, showMuteCondition) {
     } catch { }
 }
 
+function updateTimer(frameDocument, timer) {
+    // Try catch because draw() will sometimes call before document has loaded  
+    try {
+        let mins = Math.floor(timer / 60);
+        let secs = timer % 60;
+        let timerelement = frameDocument.getElementById("timer");
+        let timersecs = secs.toString();
+        let timermins = mins.toString();
+        if (secs < 10) {
+            timersecs = "0" + secs.toString();
+            console.log("less than 10")
+        }
+        if (mins < 10) {
+            timermins = "0" + mins.toString();
+        }
+        timerelement.innerHTML = timermins + ":" + timersecs;
+
+        if (timer < 60){
+            timerelement.classList.add("text-red-600");
+            // frameDocument.classList.add("outline-red-400 outline-2 outline-offset-1");
+        } else {
+            if (timerelement.classList.contains("text-red-600")) {
+                // frameDocument.classList.remove("outline-red-500 outline-2 outline-offset-1");
+                timerelement.classList.remove("text-red-600");
+            }
+        }
+
+
+        if (timer < 0) {
+            timer.innerHTML = "00:00";
+        }
+        
+    } catch { }
+}
+
 function highlightBlockDisplay(frameDocument, wallEditorMode){
     //removes backgrounds from all blocks
     let blocktext;
